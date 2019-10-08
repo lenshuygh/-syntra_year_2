be1a67d160f3f33aba6265eb2796d819024e7100
lens <lens.huygh@gmail.com>
2019-10-08 19:44:00 +0200

    fixed ex 4 binarySearch

--- src/recap_exercises/Ex04BinarySearch.java
+++ src/recap_exercises/Ex04BinarySearch.java
@@ -3,22 +3,23 @@ package recap_exercises;
 public class Ex04BinarySearch {
     public static void main(String[] args) {
         int[] sortedArrayOfInts = {2, 4, 6, 7, 10, 18, 22, 49, 55, 108, 170, 200, 221, 223, 229, 240};
-        System.out.println(returnIndexOfInt(sortedArrayOfInts, 223,0,sortedArrayOfInts.length));
+        System.out.println(returnIndexOfInt(sortedArrayOfInts, 10, 0, sortedArrayOfInts.length));
     }
 
-    private static int returnIndexOfInt(int[] sortedArrayOfInts, int searchedInt,int startIndex,int endIndex) {
-        final int middleIndex = endIndex / 2;
-        if((sortedArrayOfInts[startIndex] != searchedInt) && (sortedArrayOfInts[endIndex-1] != searchedInt) ){
-            if(searchedInt < sortedArrayOfInts[middleIndex]){
-                return returnIndexOfInt(sortedArrayOfInts,searchedInt,startIndex,middleIndex);
-            }else{
-                return returnIndexOfInt(sortedArrayOfInts,searchedInt,middleIndex,endIndex);
+    private static int returnIndexOfInt(int[] sortedArrayOfInts, int searchedInt, int startIndex, int endIndex) {
+        int middleIndex = endIndex / 2;
+        while (startIndex + 1 != endIndex) {
+            if (searchedInt < sortedArrayOfInts[middleIndex]) {
+                endIndex = middleIndex;
+                middleIndex = endIndex / 2;
+            } else {
+                startIndex = middleIndex;
+                middleIndex = middleIndex + ((endIndex - middleIndex) / 2);
             }
-        }else{
-            if(sortedArrayOfInts[startIndex] == searchedInt){
-                return startIndex;
+            if(sortedArrayOfInts[middleIndex] == searchedInt){
+                return middleIndex;
             }
-            return endIndex-1;
         }
+        return -1;
     }
 }
